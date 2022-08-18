9df8021cfa91f6e6dcbd098107bb5809f3a13bc4
chenlong <chenlong1@jkinvest.cn>
2022-08-18 15:20:21 +0800

    update

--- vite.config.ts
+++ vite.config.ts
@@ -1,12 +1,12 @@
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import Unocss from "./config/unocss";
 
 // https://vitejs.dev/config/
 
 const rollupOptions = {
-
-  external: ["vue", "vue-router"],
+external: ["vue", "vue-router"],
   output: {
     globals: {
       vue: "Vue",
@@ -15,13 +15,16 @@ const rollupOptions = {
 };
 
 export default defineConfig({
+  resolve: { alias: { "vue": "vue/dist/vue.esm-bundler.js" } },
 
   plugins: [
       vue(),
     // 添加JSX插件
     vueJsx({
       // options are passed on to @vue/babel-plugin-jsx
-    })
+    }),
+    // 添加UnoCSS插件
+    Unocss()
   ],
 
   // 添加库模式配置
